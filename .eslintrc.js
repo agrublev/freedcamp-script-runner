b927819ba782737f3fbd5fec7ef35b43e2a6d695
Angel Grablev <agrublev@gmail.com>
2019-03-25 18:45:51 -0700

    ESLINT

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,56 +2,21 @@ module.exports = {
 	globals: {
 		importScripts: true
 	},
-	plugins: ["monorepo"],
+	'parserOptions': {
+		'ecmaVersion': 8
+	},
 	env: {
-		browser: true,
 		node: true,
 		es6: true
 	},
-	rules: {
-		"no-mixed-operators": ["off"],
-		"no-console": ["off"],
-		"react/jsx-indent": ["off"],
-		"react/destructuring-assignment": ["off"],
-		"import/no-named-as-default-member": ["off"],
-		"import/named": ["off"],
-		"import/order": ["off"],
-		"import/no-extraneous-dependencies": ["off"],
-		"import/no-named-as-default": ["off"],
-		"react/jsx-indent-props": ["off"],
-		"flowtype/require-return-type": ["off"],
-		camelcase: [
-			1,
-			{
-				properties: "always"
-			}
-		],
-		"react/jsx-one-expression-per-line": [
-			"warn",
-			{
-				allow: "none"
-			}
-		],
-		"react/prop-types": "off",
-		complexity: ["warn", 5],
-		"max-nested-callbacks": ["warn", 8],
-		"no-unused-vars": "warn",
-		"max-statements": [
-			"warn",
-			{
-				max: 4
-			}
-		],
-		"max-statements-per-line": [
-			"warn",
-			{
-				max: 1
-			}
-		],
-		"getter-return": "warn",
-		"jsx-quotes": ["warn", "prefer-double"],
-		"monorepo/no-internal-import": "error",
-		"monorepo/no-relative-import": "error"
+	'ecmaFeatures': {
+		'modules': true,
+		'arrowFunctions': true,
+		'classes': true
 	},
-	extends: ["prettier", "eslint:recommended"]
-};
+	plugins: ['prettier'],
+	rules: {
+		'no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }]
+	},
+	extends: ['plugin:prettier/recommended']
+}
