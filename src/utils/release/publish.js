2446efd36ad6e3384076c9976b219e63f509bddb
Angel Grablev <agrublev@gmail.com>
2019-03-27 05:23:06 -0700

    VERSION 1.1.34 3434343

--- src/utils/release/publish.js
+++ src/utils/release/publish.js
@@ -8,7 +8,6 @@ async function pub() {
     runTask("sh " + path.join(__dirname, "publish.sh") + " --")
         .then(async () => {
             let pack = await readJson("package.json");
-            // console.log(pack.version);
             inquirer
                 .prompt([
                     {
@@ -34,18 +33,5 @@ async function pub() {
         .catch(e => {
             console.warn("-- Console ERR", e);
         });
-    // let pack = { version: "1.5.522" };
-    // let commitmsg = "%@%@";
-    //     require("simple-git")()
-    //         .add("./*")
-    //         .commit(
-    //             `VERSION ${pack.version}
-    // ${commitmsg}`
-    //         )
-    //         .push(["-u"], () => console.log("done"))
-    //         .addTag(`${pack.version}`, () => console.warn("-- Console TAGGED", 52))
-    //         .pushTags("origin", () => {
-    //             console.warn("-- Console 3", 3);
-    //         });
 }
 pub();
