6a270978ff83cbf94c7b55bf3af11b2f01f4bf5a
Angel Grablev <agrublev@gmail.com>
2019-03-27 04:26:47 -0700

    v1.1.20

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 npm version patch -f
-yarn pack
 npm publish
 npm i -g freedcamp-script-runner@latest
--- src/back/ndeval.js
+++
@@ -1,9 +0,0 @@
-const nodeEval = require("node-eval");
-
-(async () => {
-    return nodeEval(`
-	console.warn("RUNS JSUNS JSUNS JSUNS JSUNS JSUNS JSUNS JS\\n\\nUNS JSUNS JSUNS JSUNS JSUNS JS\\n\\n");
-	setTimeout(()=>{console.warn("RUNS JSUNS JSUNS JSUNS JSUNS JSUNS JSUNS JS\\n\\nUNS JSUNS JSUNS JSUNS JSUNS JS\\n\\nJSUNS JSUNS JSUNS JSUNS JSUNS JSUNS JS\\n\\nUNS JSUNS JSUNS JSUNS JSUNS JS\\n\\nJSUNS JSUNS JSUNS JSUNS JSUNS JSUNS JS\\n\\nUNS JSUNS JSUNS JSUNS JSUNS JS\\n\\nJS");},4000);
-	console.warn("RUNS JS");
-	`);
-})();
