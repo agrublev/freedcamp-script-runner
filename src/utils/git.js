3ab4a0a1f30b01095b32eb35f65ec97406a6e8b0
Angel Grablev <agrublev@gmail.com>
2019-03-27 04:27:59 -0700

    TST

--- src/utils/git.js
+++ src/utils/git.js
@@ -2,4 +2,4 @@ require("simple-git")()
     .add("./*")
     .commit("TST")
     // .addRemote("origin", "some-repo-url")
-    .push([], () => console.log("done"));
+    .push(["-u"], () => console.log("done"));
