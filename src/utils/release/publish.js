a904ebec124be0dfb81a0c6ed6fd9eaf8d1bac7e
Angel Grablev <agrublev@gmail.com>
2019-03-27 05:11:44 -0700

    VERSION 1.1.27 GREAT!!

--- src/utils/release/publish.js
+++ src/utils/release/publish.js
@@ -1,30 +1,38 @@
 var inquirer = require("inquirer");
 const chalk = require("chalk");
+const path = require("path");
 const runTask = require("../task-runner.js");
 const { readJson } = require("../index.js");
 
 async function pub() {
-     runTask("sh publish.sh --");
-    let pack = await readJson("package.json");
-    // console.log(pack.version);
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: chalk.bold.hex("#38be18")(`What's new this version ${pack.version}: `),
-                name: "commitmsg"
-            }
-        ])
-        .then(({ commitmsg }) => {
-            commitmsg;
-            require("simple-git")()
-                .add("./*")
-                .commit(
-                    `VERSION ${pack.version}
+    runTask("sh " + path.join(__dirname, "publish.sh") + " --")
+        .then(async () => {
+            let pack = await readJson("package.json");
+            // console.log(pack.version);
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: chalk.bold.hex("#38be18")(
+                            `What's new this version ${pack.version}: `
+                        ),
+                        name: "commitmsg"
+                    }
+                ])
+                .then(({ commitmsg }) => {
+                    commitmsg;
+                    require("simple-git")()
+                        .add("./*")
+                        .commit(
+                            `VERSION ${pack.version}
 ${commitmsg}`
-                )
-                .push(["-u"], () => console.log("done"))
-                .addTag(`${pack.version}`, () => console.warn("-- Console TAGGED", 52));
+                        )
+                        .push(["-u"], () => console.log("done"))
+                        .addTag(`${pack.version}`, () => console.warn("-- Console TAGGED", 52));
+                });
+        })
+        .catch(e => {
+            console.warn("-- Console ERR", e);
         });
 }
 pub();
