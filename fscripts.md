564e7577c9623c48ba94531ad2016efcae6c4322
Angel Grablev <agrublev@gmail.com>
2019-04-29 18:30:18 -0700

    V2.0.0 RUNS

---
+++ fscripts.md
@@ -0,0 +1,42 @@
+- [Group 1](#group-1)
+  * [run](#run)
+  * [run:s](#runs)
+  * [run:p](#runp)
+  * [run:one](#runone)
+  * [run:two](#runtwo)
+
+# Run
+
+Running
+
+## run:s
+
+Explain sequence
+
+```bash
+fsr run-s run:one run:three run:two
+```
+
+## run:p
+
+```bash
+yarn fsr run-p run:one run:two
+```
+
+## run:one
+
+```bash
+echo "~ ~ ONEEE"
+```
+
+## run:two
+
+```bash
+node testConsole.js
+```
+
+## run:three
+
+```bash
+node testInput.js
+```
--- index.js
+++ index.js
@@ -1,107 +1,144 @@
 #!/usr/bin/env node
 
-const fcr = require("./lib/index.js");
 const chalk = require("chalk");
-const getStream = require("get-stream");
-const execa = require("execa");
-let hasBeenCleared = false;
+const moment = require("moment");
 const runCLICommand = require("./lib/runCLICommand.js");
 const parseScriptFile = require("./lib/parseScriptsMd.js");
+const taskList = require("./lib/taskList");
+const path = require("path");
+const scriptsDir = process.cwd();
+const rootDir = path.join(scriptsDir, "./");
+const separator = "   ~   ";
 
-const pipeAsyncFunctions = (...fns) => arg => fns.reduce((p, f) => p.then(f), Promise.resolve(arg));
+const Conf = require("conf");
+const config = new Conf();
 
-const exec = async command => {
-    const output = await execa.shell(command, { env: { FORCE_COLOR: true }, cleanup: true }).stdout;
-
-    output.pipe(process.stdout);
-    output.pipe(process.stderr);
-    // getStream(output).then(value => {
-    // 	console.log('child output:', value);
-    // });
-
-    // output.
-    return output;
+const scriptsParsed = async () => {
+    const FcScripts = await parseScriptFile();
+    return FcScripts;
 };
+
+const startScripts = async () => {
+    const FcScripts = await scriptsParsed();
+    const recentTasks = config.get("recentTasks", {});
+    console.warn("-- Console REC", recentTasks);
+    let recentTaskArr = Object.keys(recentTasks).map(taskName => {
+            let task = recentTasks[taskName];
+            return { name: taskName, lastExecuted: task.lastExecuted };
+        })
+        .sort((a, b) =>
+            a.lastExecuted > b.lastExecuted ? 1 : b.lastExecuted > a.lastExecuted ? -1 : 0
+        )
+        .reverse()
+        .slice(0, 3);
+    let recentTaskOptions = recentTaskArr.map(task => {
+        return task.name + separator + moment(task.lastExecuted).calendar();
+    });
+    let taskToRun = await taskList(FcScripts, recentTaskOptions);
+    // + separator + moment().format("MMMM Do YYYY, h:mm:ss a")
+    if (recentTasks[taskToRun] === undefined) {
+        recentTasks[taskToRun] = { lastExecuted: Date.now() };
+    } else {
+        recentTasks[taskToRun].lastExecuted = Date.now();
+    }
+    config.set("recentTasks", recentTasks);
+    console.log("WILL RUN", taskToRun);
+};
+
 const argv = require("yargs")
-.usage("Usage: $0 <command> [options]")
-.scriptName("yarn fsr")
-.example(
-    `${chalk.rgb(39, 173, 96)("$0")}`,
-    `${chalk.rgb(159, 161, 181)("will open a task selection selector")}`
-)
-.command("start", "Start the scripts!", yargs => {}, async function() {
-    // let ff = await fcr(hasBeenCleared);
-    // if (!hasBeenCleared) {
-    //     hasBeenCleared = true;
-    // }
-    // ff.runList();
-    const FcScripts = await parseScriptFile();
-    console.warn("-- Console ", FcScripts);
-})
-.example(
-    `${chalk.rgb(39, 173, 96)("$0 start")}`,
-    `${chalk.rgb(159, 161, 181)("will open a task selection selector")}`
-)
-.command("run", "Run a specific task", () => {}, async function(argv) {
-    let task = argv._[1];
-    let ff = await fcr(hasBeenCleared);
-    if (!hasBeenCleared) {
-        hasBeenCleared = true;
-    }
-    await ff.runFile(task);
-})
-.example(
-    `${chalk.rgb(39, 173, 96)("$0 run start:web")}`,
-    `${chalk.rgb(159, 161, 181)("will run task 'start:web'")}`
-)
-.command("run-s", "Series as a sequence", () => {}, async function(argv) {
-    let tasks = argv._.slice();
-    tasks.shift();
-    const FcScripts = await parseScriptFile();
+    .usage("Usage: $0 <command> [options]")
+    .scriptName("yarn fsr")
+    .example(
+        `${chalk.rgb(39, 173, 96)("$0")}`,
+        `${chalk.rgb(159, 161, 181)("will open a task selection selector")}`
+    )
+    .command("start", "Start the scripts!", yargs => {}, async function() {
+        await startScripts();
+    })
+    .example(
+        `${chalk.rgb(39, 173, 96)("$0 start")}`,
+        `${chalk.rgb(159, 161, 181)("will open a task selection selector")}`
+    )
+    .command("run", "Run a specific task", () => {}, async function(argv) {
+        let task = argv._[1];
+    })
+    .example(
+        `${chalk.rgb(39, 173, 96)("$0 run start:web")}`,
+        `${chalk.rgb(159, 161, 181)("will run task 'start:web'")}`
+    )
+    .command("run-s", "Series as a sequence", () => {}, async function(argv) {
+        let tasks = argv._.slice();
+        tasks.shift();
+        const FcScripts = await parseScriptFile();
 
-    for (let t in tasks) {
-        let taskName = tasks[t];
-        let script = FcScripts.allTasks[taskName].script;
-        let params = script.split(" ");
-        let type = params.shift();
-        await runCLICommand({
-            task: { name: taskName },
-            script: {
-                type: type,
-                rest: params
-            }
-        });
-    }
-})
-.example(
-    `${chalk.rgb(39, 173, 96)("$0 run-s start:web start:desktop")}`,
-    `${chalk.rgb(159, 161, 181)("will run task 'start:web' and afterwards 'start:desktop'")}`
-)
-.command("run-p", "Run tasks in parallel", () => {}, async function(argv) {
-    let tasks = argv._.slice();
-    tasks.shift();
-    const FcScripts = await parseScriptFile();
+        for (let t in tasks) {
+            let taskName = tasks[t];
+            let script = FcScripts.allTasks[taskName].script;
+            let params = script.split(" ");
+            let type = params.shift();
+            await runCLICommand({
+                task: { name: taskName },
+                script: {
+                    type: type,
+                    rest: params
+                }
+            });
+        }
+    })
+    .example(
+        `${chalk.rgb(39, 173, 96)("$0 run-s start:web start:desktop")}`,
+        `${chalk.rgb(159, 161, 181)("will run task 'start:web' and afterwards 'start:desktop'")}`
+    ).command("clear", "Clear history", () => {}, async function(argv) {
+        let tasks = argv._.slice();
+        tasks.shift();
+        const FcScripts = await parseScriptFile();
 
-    for (let t in tasks) {
-        let taskName = tasks[t];
-        let script = FcScripts.allTasks[taskName].script;
-        let params = script.split(" ");
-        let type = params.shift();
-        runCLICommand({
-            task: { name: taskName },
-            script: {
-                type: type,
-                rest: params
-            }
-        });
-    }
-})
-.example(
-    `${chalk.rgb(39, 173, 96)("$0 run-p start:web start:desktop")}`,
-    `${chalk.rgb(159, 161, 181)(
-        "will run task 'start:web' and at the same time 'start:desktop'"
-    )}`
-).argv;
+        for (let t in tasks) {
+            let taskName = tasks[t];
+            let script = FcScripts.allTasks[taskName].script;
+            let params = script.split(" ");
+            let type = params.shift();
+            await runCLICommand({
+                task: { name: taskName },
+                script: {
+                    type: type,
+                    rest: params
+                }
+            });
+        }
+    })
+    .example(
+        `${chalk.rgb(39, 173, 96)("$0 run-s start:web start:desktop")}`,
+        `${chalk.rgb(159, 161, 181)("will run task 'start:web' and afterwards 'start:desktop'")}`
+    )
+    .command("run-p", "Run tasks in parallel", () => {}, async function(argv) {
+        let tasks = argv._.slice();
+        tasks.shift();
+        const FcScripts = await parseScriptFile();
+
+        for (let t in tasks) {
+            let taskName = tasks[t];
+            let script = FcScripts.allTasks[taskName].script;
+            let params = script.split(" ");
+            let type = params.shift();
+            runCLICommand({
+                task: { name: taskName },
+                script: {
+                    type: type,
+                    rest: params
+                }
+            });
+        }
+    })
+    .example(
+        `${chalk.rgb(39, 173, 96)("$0 run-p start:web start:desktop")}`,
+        `${chalk.rgb(159, 161, 181)(
+            "will run task 'start:web' and at the same time 'start:desktop'"
+        )}`
+    ).argv;
+
 if (argv._.length === 0) {
-    fcr().then(ff => ff.runList());
+    (async function() {
+        await startScripts();
+    })();
 }
